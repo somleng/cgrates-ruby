eea68130554d94c379cbb182e71e00a0e27ed5f6
David Wilkie <dwilkie@gmail.com>
2025-11-14 11:58:04 +0700

    Initial commit

---
+++ spec/cgrates/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cgrates::Ruby do
+  it "has a version number" do
+    expect(Cgrates::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
